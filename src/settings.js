3536fc315cf202a283d0410a69959413678aff71
Nyi Nyi <nnsoulkeeper@gmail.com>
2022-08-29 15:14:41 +0630

    first commit

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default",""],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/UnicodeHero.fire",orientation:"",jsList:[]};
